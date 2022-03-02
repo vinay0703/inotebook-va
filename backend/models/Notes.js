3baa94369dda218dddb292070054ab056d342d6c
vinay0703 <vinay.araveti@gmail.com>
2022-03-02 22:44:14 +0530

    imported {Schema} from mongoose

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const {Schema} = mongoose;
 const notesSchema = new Schema({
     title:{
         type:String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
+//Create a user using : POST "/api/auth". Does'nt require auth.
 router.get('/', (req, res)=>{
-    res.json([]);
+    console.log(req.body);
+    const user=User(req.body);
+    user.save();
+    res.send(req.body);
 });
 
 module.exports = router;
