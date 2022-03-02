c5e760277cbcad540ee04122bc1c64c64e1a83a2
vinay0703 <vinay.araveti@gmail.com>
2022-03-02 21:41:20 +0530

    Handles all the database tasks

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+const connectToMongo = async()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to mongo successfully");
+    });
+}
+
+module.exports = connectToMongo;
