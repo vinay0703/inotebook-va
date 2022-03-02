1a9896cd93451ef91dfe3f3dd70c7e5adfd57905
vinay0703 <vinay.araveti@gmail.com>
2022-03-02 22:22:06 +0530

    importing available routes from routes folder using express().use() function

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('Hello world!');
-});
+// Available routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-  })
+    console.log(`app listening at http://localhost:${port}`);
+});
