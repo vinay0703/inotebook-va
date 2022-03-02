3185ef974c366d56fdd328e05b999d149d379f31
vinay0703 <vinay.araveti@gmail.com>
2022-03-02 21:41:52 +0530

    Initiated express and mongoose server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require('./db');
+const express = require('express');
+
+connectToMongo();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+});
+
+app.listen(port, ()=>{
+    console.log('App listening at localhost', port);
+});
