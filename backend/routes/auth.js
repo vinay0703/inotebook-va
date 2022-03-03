0eba9a363a24d6d9864d1bb05ac5f95b22085c87
vinay0703 <vinay.araveti@gmail.com>
2022-03-03 11:28:02 +0530

    Added express-validators on name, email and password fields; also added functionality of checking if the user with mail already exits

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const {body, validationResult} = require('express-validator');
 
-//Create a user using : POST "/api/auth". Does'nt require auth.
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+//Create a user using : POST "/api/auth/createuser". Does'nt require auth.
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({min:3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be min of length 5').isLength({min:5}),
+], async (req, res)=>{
+    //If there are errors return Bad request and the errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    try {
+        //creating a user
+        //Check whether the email already exits.
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: 'Sorry a user with this mail already exits'});
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occurred");
+    }
 });
 
 module.exports = router;
