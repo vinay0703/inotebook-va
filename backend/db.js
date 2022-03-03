1587522b41b3913a14f0e0d6ac4c17ea17a0c337
vinay0703 <vinay.araveti@gmail.com>
2022-03-03 11:29:37 +0530

    Redirected mongoURI into inotebook database

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/inotebook";
 const connectToMongo = async()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("connected to mongo successfully");
