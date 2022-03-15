67e11d9b2b3a236acf4dfe16943a5fa299dc6355
vinay0703 <vinay.araveti@gmail.com>
2022-03-15 21:01:40 +0530

    Created login endpoint and send authToken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,9 @@ const User = require('../models/User');
 const {body, validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-
 const JWT_SECRET = "Harryisagoodb$y";
 
-//Create a user using : POST "/api/auth/createuser". Does'nt require auth.
+//Create a user using : POST "/api/auth/createuser". No login required.
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({min:3}),
     body('email', 'Enter a valid email').isEmail(),
@@ -47,11 +46,52 @@ router.post('/createuser', [
         //Using a jwt so that each user gets his appropriate token
         const authToken = jwt.sign(data, JWT_SECRET);
         res.json(authToken);
-        
+
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occurred");
+        res.status(500).send("Internal server error");
     }
 });
+
+
+//Authenticate a user using : POST "/api/auth/login".
+router.post('/login',[
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async(req, res)=>{
+    //If there are errors return Bad request and the errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please login with correct credentials."});
+        }
+
+        //comparing password using bcrypt.js
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please login with correct credentials."});
+        }
+
+        //If password is correct then we send user payload(data)
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json(authToken);
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+});
+
 
 module.exports = router;
