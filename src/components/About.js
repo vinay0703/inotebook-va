3d443c38b853fdb483f0b53f96ef5fd241b1a0b7
vinay0703 <vinay.araveti@gmail.com>
2022-03-16 17:08:23 +0530

    Generalized code by removing sample variables

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,8 @@
-import React, {useEffect, useContext} from 'react';
-import noteContext from '../context/notes/noteContext';
+import React from 'react';
 
 const About = () =>{
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-  }, [])
-  
   return (
-    <div>This is About {a.state.name} and in class {a.state.class}</div>
+    <div>This is About </div>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name":"Vinay",
-        "class":"10"
-    }
-    const [state, setState] = useState(s1);
-    const update = ()=>{
-        setTimeout(()=>{
-            setState({
-                "name":"Deshgani",
-                "class":"10"
-            });
-        }, 1000);
-    }
     return(
-        <noteContext.Provider value = {{state:state,update:update}}>
+        <noteContext.Provider value = {{}}>
             {props.children}
         </noteContext.Provider>
     ); 
