2ae29f3e2a6afb3466aafae32ec49366997531ec
vinay0703 <vinay.araveti@gmail.com>
2022-03-16 16:42:45 +0530

    Added NoteState hook (useContext hook) for notes to avoid state lift problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <h1>Welcome to inotebook-va</h1>
+    <NoteState>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path = "/about">
+          <About/>
+        </Route>
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+    </NoteState>
     </>
   );
 }
