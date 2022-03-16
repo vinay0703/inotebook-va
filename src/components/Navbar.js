d91fc302b4decbab8e70425f3d24b79eea85e625
vinay0703 <vinay.araveti@gmail.com>
2022-03-16 17:05:59 +0530

    Made Navbar dark and added useLocation hook for adding active class in Navbar to active link.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Link, useLocation} from "react-router-dom";
 
 function Navbar() {
+  let location = useLocation();
+  useEffect(()=>{
+    console.log(location.pathname);
+  }, [location]);
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <Link className="navbar-brand" to="/"><i className="fa fa-book"></i> inotebook-va</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,11 +15,11 @@ function Navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item active">
-                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === '/'?"active":""}`} aria-current="page" to="/">Home</Link>
                 </li>
-                <li className="nav-item active">
-                <Link className="nav-link" to="/about">About</Link>
+                <li className="nav-item">
+                <Link className={`nav-link ${location.pathname === '/about'?"active":""}`} to="/about">About</Link>
                 </li>
             </ul>
             <form className="d-flex">
