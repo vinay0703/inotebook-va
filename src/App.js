9f6e90a901bb12602bb80a7cf1ff53ce4feaeb2e
vinay0703 <vinay.araveti@gmail.com>
2022-03-16 17:11:39 +0530

    Aligned all the body components to center by using .container class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/notes/NoteState';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-    <NoteState>
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route path = "/about">
-          <About/>
-        </Route>
-        <Route exact path = "/">
-          <Home/>
-        </Route>
-      </Switch>
-    </Router>
-    </NoteState>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
