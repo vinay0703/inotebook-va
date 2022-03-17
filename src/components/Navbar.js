1c1e97c968a6e68a226d20e909bd133c6c9ee836
vinay0703 <vinay.araveti@gmail.com>
2022-03-17 21:09:14 +0530

    Added Contactus, Login, Signup in navbar and removed search button since it functionality is optional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,101 @@
-import React, {useEffect} from 'react';
-import {Link, useLocation} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   let location = useLocation();
-  useEffect(()=>{
-  }, [location]);
+  useEffect(() => {}, [location]);
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to="/"><i className="fa fa-book"></i> inotebook-va</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <Link className={`nav-link ${location.pathname === '/'?"active":""}`} aria-current="page" to="/">Home</Link>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <i className="fa fa-book"></i> inotebook-va
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/" ? "active" : ""
+                }`}
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className={`nav-link ${
+                  location.pathname === "/contact" ? "active" : ""
+                }`} to="/contact">
+                Contact us
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle" 
+                to="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Login/SignUp
+              </Link>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <button
+                    className="dropdown-item"
+                    data-bs-toggle="modal"
+                    data-bs-target="#loginModal"
+                  >
+                    Login
+                  </button>
                 </li>
-                <li className="nav-item">
-                <Link className={`nav-link ${location.pathname === '/about'?"active":""}`} to="/about">About</Link>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    data-bs-toggle="modal"
+                    data-bs-target="#signUpModal"
+                  >
+                    SignUp
+                  </button>
                 </li>
-            </ul>
-            <form className="d-flex">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            </div>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
