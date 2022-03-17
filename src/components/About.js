d15b5847c898137644c534a1a53d77e64d015fb5
vinay0703 <vinay.araveti@gmail.com>
2022-03-17 20:58:05 +0530

    Modifed About.js which has AboutItem component in it.

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from 'react'
+import AboutItem from './AboutItem'
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
 
-const About = () =>{
+export default function About() {
   return (
-    <div>This is About </div>
+    <div className="container">
+      <AboutItem imgSource={image1} firstHeading="Create a notebook in inotebook-va." content="inotebook-va is a digital notebook that saves and syncs your work across devices from web."/>
+      <hr />
+      {/* Swapping contentOrder and imgOrder for second AboutItem */}
+      <AboutItem contentOrder={2} imgOrder={1} imgSource={image2} firstHeading="It all started with inotebook-va idea." mutedText="We started in 2022." content="inotebook-va is a free open-source web application which was developed by Vinay Araveti."/>
+      <hr />
+      <AboutItem imgSource={image3} firstHeading="Start taking notes." content="Enter title, description, tag and click on add note button to add your notes. You can update or delete your note at any time."/>
+    </div>
   )
 }
-
-export default About;
