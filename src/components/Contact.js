4e9a3183bc3f89771117fcbf93974645bd64692c
vinay0703 <vinay.araveti@gmail.com>
2022-03-18 00:35:09 +0530

    Removed checkbox (are you a member?) and added h1 heading

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Contact() {
   return (
       <div className="container my-3">
+        <h1 className="mx-auto">Fill in the form below</h1>
+        <hr />
         <form>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Email address</label>
@@ -15,15 +17,6 @@ export default function Contact() {
           <div className="mb-3">
             <label htmlFor="concern" className="form-label">Eloborate your concern</label>
             <textarea className="form-control" id="concern" rows="3" placeholder="Enter your concern here."></textarea>
-          </div>
-          <div className="mb-3">
-            <div className="form- group row">
-              <div className="col-sm-2">Are you a member?</div>
-              <div className="col-sm-10">
-                <input className="form-check-input" type="checkbox" value="IsMember" id="IsMember"/>
-                <label className="form-check-label" htmlFor="IsMember">Yes</label>
-              </div>
-            </div>
           </div>
           <div className="mb-3">
             <button type="submit" className="btn btn-primary">Submit</button>
