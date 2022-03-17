88a0f339db67fefa3c74dfb1ef23c3d2f4f45977
vinay0703 <vinay.araveti@gmail.com>
2022-03-17 23:59:43 +0530

    Changed the description content

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ import image3 from './images/image3.jpg';
 export default function About() {
   return (
     <div className="container">
-      <AboutItem imgSource={image1} firstHeading="Create a notebook in inotebook-va." content="inotebook-va is a digital notebook that saves and syncs your work across devices from web."/>
+      <AboutItem imgSource={image1} firstHeading="Create a notebook in inotebook-va." content="inotebook-va is a digital notebook that saves and syncs your work across devices from the web."/>
       <hr />
       {/* Swapping contentOrder and imgOrder for second AboutItem */}
       <AboutItem contentOrder={2} imgOrder={1} imgSource={image2} firstHeading="It all started with inotebook-va idea." mutedText="We started in 2022." content="inotebook-va is a free open-source web application which was developed by Vinay Araveti."/>
       <hr />
-      <AboutItem imgSource={image3} firstHeading="Start taking notes." content="Enter title, description, tag and click on add note button to add your notes. You can update or delete your note at any time."/>
+      <AboutItem imgSource={image3} firstHeading="Start taking notes." content="Login to your account. Enter title, description, tag and click on add note button to add your notes. You can update or delete your note at any time."/>
     </div>
   )
 }
