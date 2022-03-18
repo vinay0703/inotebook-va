2f4cc02f872bb38ae16722169e35873b5fcfd479
vinay0703 <vinay.araveti@gmail.com>
2022-03-18 14:55:47 +0530

    imported UpdateNoteModal and changed buttons

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import noteContext from '../context/notes/noteContext';
+import UpdateNoteModal from './UpdateNoteModal';
 
 export default function NoteItem(props) {
     const context = useContext(noteContext);
@@ -9,14 +10,17 @@ export default function NoteItem(props) {
         deleteNote(note._id);
     }
     return (
+    <>
+    <UpdateNoteModal/>
     <div className="noteCard my-2 mx-2 card" style={{"width": "18rem"}}> 
         <div className="card-body"> 
             <h5 className="card-title">{note.title}</h5> 
             <h6 className="card-subtitle mb-2 text-muted">{note.tag}</h6>
             <p className="card-text"> {note.description}</p> 
-            <button className="btn btn-primary mx-1 my-1">Edit Note</button> 
-            <button className="btn btn-primary mx-1 my-1" onClick={deleteNoteHandler}>Delete Note</button> 
+            <button className="btn btn-primary mx-1 my-1" data-bs-toggle="modal" data-bs-target="#updateNoteModal">Update</button> 
+            <button className="btn btn-primary mx-1 my-1" onClick={deleteNoteHandler}>Delete</button> 
         </div> 
     </div>
+    </>
     )
 }
