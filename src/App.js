66a874553e452ba8acab17318600c714f8d15a4b
vinay0703 <vinay.araveti@gmail.com>
2022-03-18 11:33:25 +0530

    Imported Alert.js and added dismissing alert functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import NoteState from "./context/notes/NoteState";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import HomeAfterLogin from "./components/HomeAfterLogin";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type)=>{
+    setAlert({msg:message, type:type});
+    setTimeout(()=>{
+      setAlert(null);
+    }, 2000);
+  }
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
+          <Alert alert={alert}/>
           <div className="container">
             <Switch>
               <Route path = "/contact">
