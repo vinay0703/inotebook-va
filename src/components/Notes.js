7e052b219be2825e67737ee78f7b00cfec7156c1
vinay0703 <vinay.araveti@gmail.com>
2022-03-19 11:58:10 +0530

    Used UpdateNoteModal in Notes.js and added logical button to launch it

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,23 +1,36 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import noteContext from '../context/notes/noteContext';
 import AddNote from './AddNote';
 import NoteItem from './NoteItem';
+import UpdateNoteModal from './UpdateNoteModal';
 
 function Notes() {
+    //state for update note with initial values of note
+    const [note, setNote] = useState({utitle:"", udescription:"", utag:""});
     const context = useContext(noteContext);
     const {notes, getNotes} = context;
     useEffect(()=>{
         getNotes();
+        //eslint-disable-next-line
     }, []);
+    const updateNote = (currentNote)=>{
+        ref.current.click();
+        //sets the values as current note
+        setNote({utitle:currentNote.title, udescription:currentNote.description, utag:currentNote.tag});
+    }
+    const ref = useRef(null);
     return (
     <>
     <AddNote/>
+    {/* Just a programable button and it's not visible. Functionality is to launch UpdateNoteModal */}
+    <button ref={ref} type="button" className="d-none" data-bs-toggle="modal" data-bs-target="#updateNoteModal"></button>
+    <UpdateNoteModal note={note} setNote={setNote}/>
     <div className="my-3">
         <h1>Your notes</h1> 
         <hr/> 
         <div id="notes" className="d-flex justify-content-center align-content-center row container-fluid">
             {notes.map((note)=>{
-                return <NoteItem note={note} key={note._id} />
+                return <NoteItem note={note} updateNote={updateNote} key={note._id} />
             })}
         </div>
     </div> 
