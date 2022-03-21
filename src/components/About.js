0384dd965bf3b2f78d260e8dcf69c4ae7ee1590a
vinay0703 <vinay.araveti@gmail.com>
2022-03-21 21:53:00 +0530

    Corrected grammer and made sentences more meaningful

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,11 @@ export default function About() {
       <AboutItem imgSource={image1} firstHeading="Create a notebook in inotebook-va." content="inotebook-va is a digital notebook that saves and syncs your work across devices from the web."/>
       <hr />
       {/* Swapping contentOrder and imgOrder for second AboutItem */}
-      <AboutItem contentOrder={2} imgOrder={1} imgSource={image2} firstHeading="It all started with inotebook-va idea." mutedText="We started in 2022." content="inotebook-va is a free open-source web application which was developed by Vinay Araveti."/>
+      <AboutItem contentOrder={2} imgOrder={1} imgSource={image2} firstHeading="It all started with inotebook-va idea." mutedText="We all started in 2022." content="inotebook-va is an open-source web application developed by Vinay Araveti."/>
       <hr />
-      <AboutItem imgSource={image3} firstHeading="Start taking notes." content="Login to your account. Enter title, description, tag and click on add note button to add your notes. You can update or delete your note at any time."/>
+      <AboutItem imgSource={image3} firstHeading="Start taking notes." content="Login to your account. Enter title, description, tag, and click on add note button to add your notes. You can update or delete at any time."/>
       <hr />
-      <AboutItem contentOrder={2} imgOrder={1} imgSource={image4} mutedText="Only you can access your account." firstHeading="Don't worry your account is hack free." content="We used JWT authentication (HMAC Algorithm) with salt and pepper to add extra security level for your account."/>
+      <AboutItem contentOrder={2} imgOrder={1} imgSource={image4} mutedText="Only you can access your account." firstHeading="Don't worry, your account is hack free." content="We used JWT authentication (HMAC Algorithm) with salt and pepper to add an extra security level for your account."/>
     </div>
   )
 }
