6c5031f2c6fcab1b247ff7c503a89ff25b856544
vinay0703 <vinay.araveti@gmail.com>
2022-03-21 10:45:35 +0530

    added new route for contact

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // Available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+app.use('/api/contact', require('./routes/contact'));
 
 
 app.listen(port, () => {
