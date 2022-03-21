9b04b7bdc2d9b5ef739e8a8698c0c0b622d86e9d
vinay0703 <vinay.araveti@gmail.com>
2022-03-21 23:39:27 +0530

    Added path variables to run backend and frontend on the same server side

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 connectToMongo();
 const app = express();
@@ -8,6 +9,7 @@ const port = 5000;
 
 //To avoid cors error while fetch
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 //To use req.body you need to use this middleware
 app.use(express.json());
 
@@ -16,6 +18,10 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 app.use('/api/contact', require('./routes/contact'));
 
+//For any other path redirect to index.html
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
